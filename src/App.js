980ad217e984b8fd7c6b066fe0a419383714b0a5
agilanc <chandran.agilan@rabobank.nl>
2019-07-18 11:33:07 +0530

    chore: updated package for react autobind

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+     <Route/>
+    )
+  }
 }
 
-export default App;
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Page !!!</h1>
+      </div>
+    )
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home Page !!!</h1>
+      </div>
+    )
+  }
+}
+
+class Contact extends Component {
+  render() {  
+    return (
+        <div>
+          <h1>Contact Page !!!</h1>
+        </div>
+      )
+  }
+}
+
+class Route extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: window.location.hash.substr(1)
+    }
+  }
+
+  componentDidMount() { // during component render this method trigger first
+    window.addEventListener('popstate', () => {
+      this.setState({
+        route: window.location.hash.substr(1)
+      })
+    })  
+  }
+
+  render() {
+    let Link;
+    console.log(this.state.route)
+    switch(this.state.route) {
+      case '/about': Link = About;
+        break;
+      case '/contact': Link = Contact;
+        break;
+      default: Link = Home;
+    }
+    return(
+      <div>
+        <ul>          
+        <li>
+          <a href='#'>Home</a>
+          </li>
+          <li>
+            <a href='#/about'>About</a>
+          </li>
+          <li>
+          <a href='#/contact'>Contact</a>
+          </li>
+        </ul>
+      <Link/>     
+      </div>
+    )
+  }
+}
